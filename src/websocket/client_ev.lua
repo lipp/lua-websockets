199f309cb25697036c4ae277faed755be8827e5d
Gerhard Lipp <gelipp@gmail.com>
2013-02-08 11:08:42 +0100

    minor interface change

--- src/websocket/client_ev.lua
+++ src/websocket/client_ev.lua
@@ -12,36 +12,42 @@ local ev = function(ws)
    if protocol ~= 'ws' then
       error('Protocol not supported:'..protocol)
    end
-   local sock = socket.tcp()
-   -- set non blocking
-   sock:settimeout(0)
-
+   local sock
    local loop = ws.loop or ev.Loop.default
-   local fd = sock:getfd()
-   local self = {
-      on_error = function(s,err) print('Websocket unhandled error',s,err) end,
-      on_message = function() end,
-      on_close = function() end,
-      on_connect = function() end,
-   }  
+   local fd
+   local message_io
+   local on_message
+   local on_error = function(s,err) print('Websocket unhandled error',s,err) end
+   local on_close = function() end
+   local on_connect = function() end
+   local self = {}
 
+   local send_buffer
    send = function(data,on_sent)
-      local len = #data
+      if send_buffer then
+         -- a write io is still running
+         send_buffer = send_buffer..data
+         return 
+      else
+         send_buffer = data
+      end
       local index
       ev.IO.new(
          function(loop,write_io)
-            local sent,err = sock:send(data,index)
+            local len = #send_buffer
+            local sent,err = sock:send(send_buffer,index)
             if not sent then
                write_io:stop(loop)
                if err == 'closed' then
-                  self.on_close(self)
+                  on_close(self)
                end
-               self.on_error(self,'Websocket write failed '..err)
+               on_error(self,'Websocket write failed '..err)
             elseif sent == len then
+               send_buffer = nil
                write_io:stop(loop)
-                  if on_sent then
-                     on_sent()
-                  end
+               if on_sent then
+                  on_sent()
+               end
             else
                assert(sent < len)
                index = sent
@@ -53,10 +59,14 @@ local ev = function(ws)
       local encoded = frame.encode(message,opcode or frame.TEXT)
       send(encoded)
    end
-  
-   local message_io
    
-   local connect = function()
+   local connect = function(_,on_connect_arg)
+      assert(not sock)
+      sock = socket.tcp()
+      fd = sock:getfd()
+      -- set non blocking
+      sock:settimeout(0)
+      on_connect = on_connect_arg or on_connect
       ev.IO.new(
          function(loop,connect_io)
             connect_io:stop(loop)
@@ -85,7 +95,7 @@ local ev = function(ws)
                               end
                               resp[#resp+1] = line
                            elseif err ~= 'timeout' then
-                              self.on_error(self,'Websocket Handshake failed due to socket err:'..err)
+                              on_error(self,'Websocket Handshake failed due to socket err:'..err)
                            else
                               last = part
                               return
@@ -97,16 +107,16 @@ local ev = function(ws)
                         local expected_accept = handshake.sec_websocket_accept(key)
                         if headers['sec-websocket-accept'] ~= expected_accept then
                            local msg = 'Websocket Handshake failed: Invalid Sec-Websocket-Accept (expected %s got %s)'
-                           self.on_error(self,msg:format(expected_accept,headers['sec-websocket-accept'] or 'nil'))              
+                           msg = msg:format(expected_accept,headers['sec-websocket-accept'] or 'nil')
+                           on_error(self,msg)
                            return
                         end                       
-                        self.on_connect(self)
+                        on_connect(self)
                         local last
                         local frames = {}
                         message_io = ev.IO.new(
                            function(loop,message_io)
                               local encoded,err,part = sock:receive(4096)
---                              print('XXXXXXXXXXXXXXXXX',encoded and #encoded,err,#part,last)
                               if encoded or #part > 0 then
                                  if last then
                                     encoded = last..(encoded or part)
@@ -115,39 +125,61 @@ local ev = function(ws)
                                     encoded = encoded or part
                                  end
                               elseif err ~= 'timeout' then                                 
-                                 self.on_error(self,'Websocket message read io failed: '..err)
+                                 on_error(self,'Websocket message read io failed: '..err)
                                  message_io:stop(loop)
                                  return
                               end
                               
                               repeat
                                  local decoded,fin,opcode,bytes = frame.decode(encoded)
-                       --          print('BLABLABLABLABLA',decoded,fin)
                                  if decoded then
                                     tinsert(frames,decoded)
                                     encoded = encoded:sub(bytes)
                                  end
-                                 if fin == true then                                 
-                                    self.on_message(self,tconcat(frames),opcode)
+                                 if fin == true then
+                                    on_message(self,tconcat(frames),opcode)
                                     frames = {}
                                  end
                               until not decoded
                               last = encoded
                            end,fd,ev.READ)
-                        message_io:start(loop)
+                        if on_message then
+                           message_io:start(loop)
+                        end
                      end,fd,ev.READ):start(loop) -- handshake
                end)
          end,fd,ev.WRITE):start(loop) -- connect
+      local _,err = sock:connect(host,port)
+      assert(_ == nil)
+      if err ~= 'timeout' then
+         error('Websocket could not connect to '..ws.url)
+      end
    end
-   local _,err = sock:connect(host,port)
-   assert(_ == nil)
-   if err ~= 'timeout' then
-      error('Websocket could not connect to '..ws.url)
+
+   self.on_close = function(on_close_arg)
+      on_close = on_close_arg
    end
+
+   self.on_error = function(on_error_arg)
+      on_error = on_error_arg
+   end
+
+   self.on_connect = function(on_connect_arg)
+      on_connect = on_connect_arg
+   end
+
+   self.on_message = function(on_message_arg)
+      if not on_message and message_io then
+         message_io:start(loop)
+      end
+      on_message = on_message_arg      
+   end
+   
    self.close = function()
       message_io:stop(loop)
       sock:shutdown()
       sock:close()
+      sock = nil
    end
    self.connect = connect
    return self
