6f39aabdcce2456fc30c32bcf1eed78f4dc050c0
Gerhard Lipp <gelipp@gmail.com>
2013-02-11 09:31:14 +0100

    remove noise

--- test-server/test-server-ev.lua
+++ test-server/test-server-ev.lua
@@ -1,5 +1,6 @@
 --- lua websocket equivalent to test-server.c from libwebsockets.
 -- using lua-ev event loop 
+package.path = package.path..';../src/?.lua'
 local ev = require'ev'
 local loop = ev.Loop.default
 local server = require'websocket'.server.ev.listen
@@ -7,20 +8,18 @@ local server = require'websocket'.server.ev.listen
    protocols = {
       ['lws-mirror-protocol'] = function(ws)
 	 ws:on_message(
-	    function(ws,data)
-	       ws:send(data)
---	       ws:broadcast(data)
+	    function(ws,data)	       
+	       ws:broadcast(data)
 	    end)
       end,
       ['dumb-increment-protocol'] = function(ws)	       
 	 local number = 0
 	 local timer = ev.Timer.new(
 	    function()
---	       print(number)
 	       ws:send(tostring(number))
 	       number = number + 1
 	    end,0.1,0.1)
---	 timer:start(loop)
+	 timer:start(loop)
 	 ws:on_message(
 	    function(ws,message)
 	       if message:match('reset') then
@@ -36,5 +35,7 @@ local server = require'websocket'.server.ev.listen
    port = 12345
 }
 
+print('Open browser:')
+print('file://'..io.popen('pwd'):read()..'/index.html')
 loop:loop()
 
