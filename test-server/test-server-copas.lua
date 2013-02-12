6fb86e4140d6c8fd96de227aed5175db8a5dc528
Gerhard Lipp <gelipp@gmail.com>
2013-02-12 14:38:45 +0100

    make dumb-incrmenet-protocol work

--- test-server/test-server-copas.lua
+++ test-server/test-server-copas.lua
@@ -3,6 +3,10 @@
 -- using lua-copas copasent loop 
 package.path = '../src/?.lua;../src/?/?.lua;'..package.path
 local copas = require'copas'
+local socket = require'socket'
+
+local inc_clients = {}
+
 local server = require'websocket'.server.copas.listen
 {
    protocols = {
@@ -12,25 +16,40 @@ local server = require'websocket'.server.copas.listen
 	    ws:broadcast(msg)
 	 end
       end,
-      ['dumb-increment-protocol'] = function(ws)	       
-	 local number = 0
-	 
-	 -- ws:on_message(
-	 --    function(ws,message)
-	 --       if message:match('reset') then
-	 -- 	  number = 0
-	 --       end
-	 --    end)
-	 -- ws:on_close(
-	 --    function()
-	 --       timer:stop(loop)
-	 --    end)
+      ['dumb-increment-protocol'] = function(ws)
+         local inc_client = {
+            number = 0,
+            ws = ws
+         }
+         table.insert(inc_clients,inc_client)
+         while true do            
+            local message = ws:receive()            
+            if message:match('reset') then
+               inc_client.number = 0
+            end
+         end
       end
    },
    port = 12345
 }
 
+copas.addthread(
+   function()
+      local last = socket.gettime()
+      while true do             
+         copas.step(0.1)
+         local now = socket.gettime()
+         if (now - last) >= 0.1 then
+            last = now
+            for _,inc_client in pairs(inc_clients) do
+               inc_client.number = inc_client.number + 1
+               inc_client.ws:send(tostring(inc_client.number))
+            end
+         end
+      end
+   end)
+
 print('Open browser:')
 print('file://'..io.popen('pwd'):read()..'/index.html')
-copas.loop()
 
+copas.loop()
