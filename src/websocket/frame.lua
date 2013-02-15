13d2dc7b0b95177160dd2ad9eceec1935d22581b
Gerhard Lipp <gelipp@gmail.com>
2013-02-15 11:23:12 +0100

    xor in chunks to prevent stack overflow

--- src/websocket/frame.lua
+++ src/websocket/frame.lua
@@ -11,7 +11,9 @@ local srep = string.rep
 local ssub = string.sub
 local sbyte = string.byte
 local schar = string.char
-
+local tinsert = table.insert
+local tconcat = table.concat
+local mmin = math.min
 
 local bits = function(...)
   local n = 0
@@ -26,13 +28,22 @@ local bit_0_3 = bits(0,1,2,3)
 local bit_0_6 = bits(0,1,2,3,4,5,6)
 
 local xor_mask = function(encoded,mask,payload)
-  local transformed = {}
-  local original = {sbyte(encoded,1,payload)}
-  for i=1,#original do
-    local j = (i-1) % 4 + 1
-    transformed[i] = bxor(original[i],mask[j])
+  local transformed_arr = {}
+  -- xor chunk-wise to prevent stack overflow.
+  -- sbyte and schar multiple in/out values
+  -- which require stack
+  for p=1,payload,2000 do
+    local transformed = {}
+    local last = mmin(p+1999,payload)
+    local original = {sbyte(encoded,p,last)}
+    for i=1,#original do
+      local j = (i-1) % 4 + 1
+      transformed[i] = bxor(original[i],mask[j])
+    end
+    local xored = schar(unpack(transformed))
+    tinsert(transformed_arr,xored)
   end
-  return schar(unpack(transformed))
+  return tconcat(transformed_arr)
 end
 
 local encode = function(data,opcode,masked,fin)
