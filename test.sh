fdd26f3568020c7e9c99951d1dd2acd0bef4c18f
Gerhard Lipp <gelipp@gmail.com>
2013-03-06 17:37:55 +0100

    make test serial (since autobahn fails to run multiple instances in parallel)

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 killall wstest 2>/dev/null
-wstest -m echoserver -w ws://localhost:${LUAWS_WSTEST_PORT:=8081} 1>/dev/null &
+wstest -m echoserver -w ws://localhost:${LUAWS_WSTEST_PORT:=8081}  &
 pid=$!
 sleep 1
 busted spec/
