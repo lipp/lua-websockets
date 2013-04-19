e224ea1dbf4f2bef5be17e93ebe60cfdb71e8441
Gerhard Lipp <gelipp@gmail.com>
2013-04-19 15:51:23 +0200

    add coverage output

--- test.sh
+++ test.sh
@@ -3,8 +3,10 @@ killall wstest 2>/dev/null
 wstest -m echoserver -w ws://localhost:${LUAWS_WSTEST_PORT:=8081}  &
 pid=$!
 sleep 3
-busted spec/
+rm luacov.* 2>/dev/null
+busted -c spec/
 bustedcode=$?
+egrep "\s+src/" luacov.report.out
 kill ${pid}
 exit $bustedcode
 
