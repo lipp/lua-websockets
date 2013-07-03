5fea4bd92fa97472f20d9913859fd29d13828b7f
Gerhard Lipp <gelipp@gmail.com>
2013-07-03 14:37:44 +0200

    disable coverage output

--- test.sh
+++ test.sh
@@ -2,12 +2,10 @@
 killall wstest 2>/dev/null
 wstest -m echoserver -w ws://localhost:${LUAWS_WSTEST_PORT:=8081}  &
 pid=$!
+echo "Waiting for wstest to start..."
 sleep 3
-rm luacov.* 2>/dev/null
-busted -c spec/
+busted spec/
 bustedcode=$?
-echo "code coverage"
-tail -n 15 luacov.report.out
 kill ${pid}
 exit $bustedcode
 
