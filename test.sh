406ca210ab1689c52cb4977a3d405d32e64f0c2f
Gerhard Preuss <gelipp@gmail.com>
2013-12-05 10:12:36 +0100

    increase wait time after starting autobahn

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@ killall wstest 2>/dev/null
 wstest -m echoserver -w ws://localhost:${LUAWS_WSTEST_PORT:=8081}  &
 pid=$!
 echo "Waiting for wstest to start..."
-sleep 3
+sleep 10
 busted spec/
 bustedcode=$?
 kill ${pid}
