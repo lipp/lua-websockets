5f6a86fd712e77e696fb43736043a24671c4f2f3
Gerhard Preuss <gelipp@gmail.com>
2014-06-02 22:02:46 +0200

    more travis

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 killall wstest 2>/dev/null
-wstest -m echoserver -w ws://localhost:${LUAWS_WSTEST_PORT:=8081}  &
+wstest -m echoserver -w ws://localhost:${LUAWS_WSTEST_PORT:=11000}  &
 pid=$!
 echo "Waiting for wstest to start..."
 sleep 10
