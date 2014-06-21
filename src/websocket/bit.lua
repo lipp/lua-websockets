21f66e42aaefdffb736f3fdef09c14773d2cf7c1
Nicola Fontana <ntd@entidi.it>
2014-06-21 22:36:35 +0200

    Missing compatibility for Lua 5.2 bit operations
    In the standard library provided by Lua 5.2, bit.rol and bit.rol are
    named bit32.lrotate and bit32.rrotate.

--- src/websocket/bit.lua
+++ src/websocket/bit.lua
@@ -1,6 +1,8 @@
 local has_bit32,bit = pcall(require,'bit32')
 if has_bit32 then
   -- lua 5.2
+  bit.rol = bit32.lrotate
+  bit.ror = bit32.rrotate
   return bit
 else
   -- luajit / lua 5.1 + luabitop
