f813f36b8722539e09469ab2df479bf86193148e
Gerhard Preuss <gelipp@gmail.com>
2014-06-24 09:22:16 +0200

    add performance test for encoding messages

---
+++ perf/encode_perf.lua
@@ -0,0 +1,25 @@
+#!/usr/bin/env lua
+local frame = require'websocket.frame'
+local socket = require'socket'
+local encode = frame.encode
+local TEXT = frame.TEXT
+local s = string.rep('abc',100)
+
+local tests = {
+  ['---WITH XOR---'] = true,
+  ['---WITHOUT XOR---'] = false
+}
+
+for name,do_xor in pairs(tests) do
+  print(name)
+  local n = 1000000
+  local t1 = socket.gettime()
+  for i=1,100000 do
+    encode(s,TEXT,do_xor)
+  end
+  local dt = socket.gettime() - t1
+  print('n=',n)
+  print('dt=',dt)
+  print('ops/sec=',n/dt)
+  print('microsec/op=',1000000*dt/n)
+end
