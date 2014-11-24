52ec62afc1f56469f2ebfd85a0011976df5e58b8
Gerhard Preuss <gelipp@gmail.com>
2014-11-24 09:43:53 +0100

    install luacov-coveralls, report lcov

--- test.sh
+++ test.sh
@@ -3,9 +3,8 @@ killall wstest 2>/dev/null
 wstest -m echoserver -w ws://localhost:${LUAWS_WSTEST_PORT:=11000}  &
 pid=$!
 echo "Waiting for wstest to start..."
-sleep 10
-busted spec/
+sleep 5
+busted -c spec/
 bustedcode=$?
 kill ${pid}
 exit $bustedcode
-
