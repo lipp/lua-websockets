c5a6b3044cf469adab34f82733c614d8e52a0fbf
NovusTheory <asladich@gmail.com>
2016-05-03 18:00:03 +0000

    Revert "WSS Support for ev"
    This reverts commit 0dbdeb8b06e8fa0de4a352fdf570c82755795d69.

--- src/websocket/ev_common.lua
+++ src/websocket/ev_common.lua
@@ -113,10 +113,6 @@ local message_io = function(sock,loop,on_message,on_error)
       if err then
         if err == 'timeout' and #part == 0 then
           return
-        elseif err == 'wantread' and #part == 0 then
-          return
-        elseif err == 'wantwrite' and #part == 0 then
-          return
         elseif #part == 0 then
           if message_io then
             message_io:stop(loop)
